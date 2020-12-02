25bdc7b533e0f6df63af5adc97380753817a04d6
ajiYudhanto <aji.yudhanto@gmail.com>
2020-12-03 01:13:01 +0700

    pathlib module

--- modules/app.py
+++ modules/app.py
@@ -3,14 +3,25 @@
 # numbers = [1, 2, 3, 85, 32, 32, 34, 12243, 54, 6567]
 # print(find_max(numbers))
 
-import random
+# import random
 
-class Dice:
-    def roll(self):
-        dice_1 = random.randint(1, 6)
-        dice_2 = random.randint(1, 6)
-        return (dice_1, dice_2)
+# class Dice:
+#     def roll(self):
+#         dice_1 = random.randint(1, 6)
+#         dice_2 = random.randint(1, 6)
+#         return (dice_1, dice_2)
 
 
-dice = Dice()
-print(dice.roll())
+# dice = Dice()
+# print(dice.roll())
+
+from pathlib import Path
+
+# path = Path('arg')
+# path.mkdir()
+# path.rmdir()
+
+path = Path()
+
+for file in path.glob('*.*'):
+    print(file)
