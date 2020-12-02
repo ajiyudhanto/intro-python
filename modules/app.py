272d704e86d021ca8cff2ff78d83292989a551d4
ajiYudhanto <aji.yudhanto@gmail.com>
2020-12-03 00:59:29 +0700

    default module

--- modules/app.py
+++ modules/app.py
@@ -1,4 +1,16 @@
-from utils import find_max
+# from utils import find_max
 
-numbers = [1, 2, 3, 85, 32, 32, 34, 12243, 54, 6567]
-print(find_max(numbers))
+# numbers = [1, 2, 3, 85, 32, 32, 34, 12243, 54, 6567]
+# print(find_max(numbers))
+
+import random
+
+class Dice:
+    def roll(self):
+        dice_1 = random.randint(1, 6)
+        dice_2 = random.randint(1, 6)
+        return (dice_1, dice_2)
+
+
+dice = Dice()
+print(dice.roll())
